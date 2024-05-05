6a597d0199a004be8095dec70c71c5e3db3f27dd
Slava Shcherbakov <ddecimuss@gmail.com>
2024-05-06 00:20:41 +0500

    feat: firtst component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function App() {
   return (
     <main>
+      <div className="bg-slate-50">
+        <section>
+          <MaxWidthWrapper className="pb-24 pt-10 lg:gird lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+            <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+              <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+                <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                  <img src="/snake-1.png" alt="snake" className="w-full" />
+                </div>
+                <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl" >Your image on a   <span>Custom</span> Phone Case</h1>
+              </div>
+            </div>
+          </MaxWidthWrapper>
+        </section>
+      </div>
       <h1>Test</h1>
     </main>
   );
